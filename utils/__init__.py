6c69b3e0e0ba8562fcf6a6538b9cf62d5f9aea72
James B. Pollack, MFA <james.b.pollack@gmail.com>
2025-06-08 02:25:47 -0700

    Optimize data collection

--- utils/__init__.py
+++ utils/__init__.py
@@ -8,5 +8,5 @@ from .oracles import (
     get_oracle,
     register_oracle,
 )
-from .stats import gini
+from .stats import gini, in_degree_centrality
 from .metric_plugins import register_metric, load_metric_plugins, watch_metric_plugins
